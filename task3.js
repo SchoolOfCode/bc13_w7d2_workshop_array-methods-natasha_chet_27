99ce1ad029826a861e9055792d9dd808758e56d4
Chet Jogia <chetjogia@gmail.com>
2022-11-08 12:19:40 +0000

    task 3

--- task3.js
+++ task3.js
@@ -1,1 +1,14 @@
-let someNumbers = [4, 5, 6, 78, 2, 3, 45, 34, 2, 23, 5, 45, 6, 7, 23];
+let someNumbers = [4, 5, 6, 78, 2, 3, 45, 34, 2, 90, 5, 45, 6, 7, 23];
+
+let isMultipleOfThree = someNumbers.some(element=> element%3===0)
+
+
+let isMultipleOfFive = someNumbers.some(element=> element%5===0)
+
+
+let isMultipleOfSixty = someNumbers.some(element=> element%60===0)
+
+
+let isMultipleOfNinety = someNumbers.some(element=> element%90 === 0)
+
+console.log(isMultipleOfThree,isMultipleOfFive,isMultipleOfSixty,isMultipleOfNinety)
